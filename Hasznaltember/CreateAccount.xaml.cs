935a962a5c6c345b5fea5f8e96fbb1fb59a3b546
papp.zsombor <papp.zsombor@students.jedlik.eu>
2023-03-10 12:11:10 +0100

    kezdetleges regisztráció

--- Hasznaltember/CreateAccount.xaml.cs
+++ Hasznaltember/CreateAccount.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Hasznaltember
 {
@@ -23,11 +24,34 @@ namespace Hasznaltember
         {
             InitializeComponent();
         }
-
         private void btnCreateAccount_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Kezdődhet a vadászat, grrr");
-            MessageBox.Show("Majom");
+            
+            
+            if(true/*nincs regisztrálva ilyen név(kell hozzá a kopasz)*/)
+            {
+                string username = tbUsernameCreate.Text;
+            }
+            else if (true/*van regisztrálva ilyen név(kell hozzá a kopasz)*/)
+            {
+                MessageBox.Show("Foglalt felhasználónév te Majom");
+            }
+            if (tbEmailCreate.Text.Contains("@")/*&& nincs regisztrálva ilyen email(kell hozzá a kopasz)*/)
+            {
+                string email = tbEmailCreate.Text;
+            }
+            else if (!tbEmailCreate.Text.Contains("@")/*|| nincs regisztrálva ilyen email(kell hozzá a kopasz)*/)
+            {
+                MessageBox.Show("Nem valós email te Majom");
+            }
+            if(tbPasswordCreate.Text == tbPasswordCreateRepeat.Text)
+            {
+                string password = tbPasswordCreate.Text;
+            }
+            else if(tbPasswordCreate.Text != tbPasswordCreateRepeat.Text)
+            {
+                MessageBox.Show("Jelszó nem egyezik te Majom");
+            }
         }
     }
 }
