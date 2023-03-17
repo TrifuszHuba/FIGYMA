69d3f31a8ad31f92f756542250067afcee72cc41
[Trifusz Huba] <[trifusz.huba@students.jedlik.eu]>
2023-03-17 08:46:45 +0100

    create and download multiple images

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 import os
 import openai
 import requests
+import base64
 
 f = open('test_options.txt', 'r')
 input = f.read()
 f.close()
 
-openai.api_key = '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'
+openai.api_key = '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'
 response = openai.Image.create(
     prompt=input,
-    n=1,
-    size="1024x1024"
+    n=5,
+    size="1024x1024",
+    response_format="b64_json"
 )
-image_url = response['data'][0]['url']
+for i in range(5):
+    image_json = response['data'][i]['b64_json']
 
-img_data = requests.get(image_url).content
-with open('test.jpg', 'wb') as handler:
-    handler.write(img_data)
-
-print(image_url)
+    img_data = base64.urlsafe_b64decode(image_json)
+    with open(f'img{i}.jpg', 'wb') as handler:
+        handler.write(img_data)
 
 # a *tomeg*, *kivitel*, *modell*, *evjarat* *tipus* with *teto szin*
