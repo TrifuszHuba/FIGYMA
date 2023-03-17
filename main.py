c1ca6bd8763846b1cf66b222e8293f0a3a06e208
[Trifusz Huba] <[trifusz.huba@students.jedlik.eu]>
2023-03-17 09:08:56 +0100

    ai error handling

--- main.py
+++ main.py
@@ -6,19 +6,24 @@ import base64
 f = open('test_options.txt', 'r')
 input = f.read()
 f.close()
+try:
+    openai.api_key = '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'
+    response = openai.Image.create(
+        prompt=input,
+        n=5,
+        size="1024x1024",
+        response_format="b64_json"
+    )
+    for i in range(5):
+        image_json = response['data'][i]['b64_json']
 
-openai.api_key = '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'
-response = openai.Image.create(
-    prompt=input,
-    n=5,
-    size="1024x1024",
-    response_format="b64_json"
-)
-for i in range(5):
-    image_json = response['data'][i]['b64_json']
+        img_data = base64.urlsafe_b64decode(image_json)
+        with open(f'img{i}.jpg', 'wb') as handler:
+            handler.write(img_data)
 
-    img_data = base64.urlsafe_b64decode(image_json)
-    with open(f'img{i}.jpg', 'wb') as handler:
-        handler.write(img_data)
+    # a *tomeg*, *kivitel*, *modell*, *evjarat* *tipus* with *teto szin*
+except openai.error.AuthenticationError:
+    print("Incorrect API key provided.")
 
-# a *tomeg*, *kivitel*, *modell*, *evjarat* *tipus* with *teto szin*
+except:
+    print("Unknown error has occured.")
