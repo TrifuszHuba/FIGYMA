76a9c3646b472551cb47731b71205c5bd2da695c
[Trifusz Huba] <[trifusz.huba@students.jedlik.eu]>
2023-04-14 11:40:50 +0200

    asd

---
+++ scripts/fade_in_on_scroll.js
@@ -0,0 +1,17 @@
+let elementsArray = document.querySelectorAll(".block");
+console.log(elementsArray);
+window.addEventListener('scroll', fadeIn);
+function fadeIn() {
+    console.clear();
+    for (var i = 0; i < elementsArray.length; i++) {
+        var elem = elementsArray[i]
+        var distInView = elem.getBoundingClientRect().top - window.innerHeight + 100;
+        console.log(distInView);
+        if (distInView < 0) {
+            elem.classList.add("in_view");
+        } else {
+            elem.classList.remove("in_view");
+        }
+    }
+}
+fadeIn();
