e2630cf95a3ae2c72243f461343e9fadf694dce7
Huba Trifusz <huba.trifusz@gmail.com>
2023-04-15 12:55:39 +0200

    fade in working class:block

--- scripts/fade_in_on_scroll.js
+++ scripts/fade_in_on_scroll.js
@@ -5,11 +5,12 @@ function fadeIn() {
     console.clear();
     for (var i = 0; i < elementsArray.length; i++) {
         var elem = elementsArray[i]
-        var distInView = elem.getBoundingClientRect().top - window.innerHeight + 100;
+        var distInView = elem.getBoundingClientRect().top - window.innerHeight + 40;
         console.log(distInView);
         if (distInView < 0) {
             elem.classList.add("in_view");
-        } else {
+        } 
+        else {
             elem.classList.remove("in_view");
         }
     }
