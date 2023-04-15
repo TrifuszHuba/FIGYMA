d234b2738bcd880fee5cef2b0af0fae42b7709ad
Huba Trifusz <huba.trifusz@gmail.com>
2023-04-15 12:22:12 +0200

    project block

--- scripts/title_animation.js
+++ scripts/title_animation.js
@@ -1,8 +1,3 @@
 window.addEventListener('scroll', () => {
     document.body.style.setProperty('--scroll', window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
 }, false);
-
-document.getElementsByTagName('body')[0].onscroll = () => {
-    console.clear();
-    console.log(window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
-};
